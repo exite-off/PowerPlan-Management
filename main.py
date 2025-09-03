b267f272ce0ce63184b8951cbbd31a2b545de096
exite-off <vfedin868@gmail.com>
2025-09-03 14:48:46 +0300

    Added auto-detection of system power plans.

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
-﻿from subprocess import run, CalledProcessError
+﻿import subprocess
 import pystray
 import PIL.Image
+from sys import exit as sys_exit
+import re
+
+# get available power plans from system
+def get_power_plans() -> dict[str: str]:
+    pattern = r'GUID:\s*([a-fA-F0-9-]+)\s*\(([^)]+)\)' # regex pattern to match GUID and plan name
+    plans: dict[str: str] = {}
+    result = subprocess.run(['powercfg', '-list'], capture_output=True, text=True)
+    matches = re.findall(pattern, result.stdout)
+    for match in matches:
+        plans[match[1]] = match[0]
+    return plans
 
 # dictionary of local power plans with their GUIDs
-power_plans: dict[str: str] = {
-    "Chill": "01886991-ed76-4326-b2ad-3cd3c74446cd",
-    "Performance": "e1abddeb-7848-4424-8b48-d4e9e17b3634"
-}
+power_plans: dict[str: str] = get_power_plans()
 
 # load tray icon
 image = PIL.Image.open("tray_icon.png")
 
 # set the specified power plan
-def set_power_plan(plan):
+def set_power_plan(plan_name: str) -> None:
     try:
-        run(['powercfg', '/setactive', power_plans[plan]], check=True)
+        subprocess.run(['powercfg', '/setactive', power_plans[plan_name]], check=True)
     except subprocess.CalledProcessError as e:
         with open("error.log", "a") as log_file:
             log_file.write(f"Error setting power plan {plan}: {e}\n")
 
 # menu items handler
 def on_click(tray, item):
-    match item.text:
-        case "Chill":
-            set_power_plan("Chill")
-        case "Performance":
-            set_power_plan("Performance")
-        case "Quit":
-            tray.stop()
-            exit(0)
+    if item.text == "Quit":
+        tray.stop()
+    set_power_plan(item.text)
+
+# generate menu items
+menu_items = []
+
+for plan, guid in power_plans.items():
+    menu_items.append(pystray.MenuItem(plan, on_click))
+
+menu_items.append(pystray.MenuItem("Quit", on_click))
 
 # menu setup
-menu = pystray.Menu(
-    pystray.MenuItem("Performance", on_click),
-    pystray.MenuItem("Chill", on_click),
-    pystray.MenuItem("Quit", on_click)
-)
+menu = pystray.Menu(*menu_items)
 
 # create and run the tray icon
 tray_icon = pystray.Icon("PowerPlan", image, menu = menu)
